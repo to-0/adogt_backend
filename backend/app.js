01819e675f1a6f4e019e03969d78c68c47384a77
Tomáš Kalný <xkalny@stuba.sk>
2022-04-08 16:28:52 +0200

    Uprava bugu vytvarani terminu na vencenie

--- backend/app.js
+++ backend/app.js
@@ -432,6 +432,7 @@ app.post('/forms/create', (req,res)=>{
     dog_id = req.body.dog_id;
     type = req.body.type;
     details = req.body.details;
+    console.log("TYPE",type)
     if(details == undefined || dog_id == undefined || type==undefined){
         res.status(404).json({"message": "Not all attributes provided"});
         return;
@@ -455,24 +456,37 @@ app.post('/forms/create', (req,res)=>{
             if(term_id == undefined){
                 db.any("DELETE FROM forms WHERE id=$1",[data.id])
                 .then((data)=>{
+                    console.log("Idem posielat ze atributy nie su poskytnute")
                     res.status(400).json({"message": "Not all attributes provided"});
+                    return; 
                 })
                 .catch((error)=>{
+                    console.log("Idem posielat ze nieco sa pokazilo po tych atributoch")
                     res.status(404).json({"message": "No data to be deleted and not all attributes provided"});
-                }) 
+                    return; 
+                })
+                
             }
-            db.one("UPDATE terms SET form_id=$1 WHERE terms.id=$2 RETURNING id",[data.id,term_id])
+            db.one("UPDATE terms SET form_id=$1, free=false WHERE terms.id=$2 RETURNING id",[data.id,term_id])
             .then((data)=>{
-                res.status(200).json({"message":"OK"})
+                console.log("IDem posielat ok, upravili sme aj termin")
+                res.status(200).json({"message":"OK tu"})
+                return;
             })
             .catch((error)=>{
+                console.log("Idem poslat, ze ziadne data neboli updatnute neviem preco")
                 res.status(404).json({"message": "No data to be updated"});
+                return;
             })
         }
-        res.status(200).json({"message": "OK"});
+        else{
+            res.status(200).json({"message": "OK"});
+            return;
+        }
     })
     .catch((error)=>{
         res.status(400).json({"message": "No data found"});
+        return;
     })
 })
 //načítanie detailu formulára
